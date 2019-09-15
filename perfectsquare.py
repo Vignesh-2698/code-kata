9a54625cb35ea2be36ed71f37777135b455135ee
Vignesh-2698 <53395992+Vignesh-2698@users.noreply.github.com>
2019-09-15 11:43:40 +0530

    Update perfectsquare.py

--- perfectsquare.py
+++ perfectsquare.py
@@ -1,7 +1,7 @@
-N=int(input())
-M=int(input())
-if(N==M):
-    square=N*M
-    print("yes")
-else:
-    print("no")
+N,M=(map(int,input().split()))
+square=N*M
+if(square%N==0 and square%M==0):
+    if(N==M):
+        print("yes")
+    else:
+        print("no")
